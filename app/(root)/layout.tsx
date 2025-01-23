1686f5052d5208d62a710ca6328e436e40b70231
Arun <ak3930566@gmail.com>
2025-01-23 11:28:40 +0530

    Fix missing id for script in (root)/layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,11 +10,12 @@ export default function PagesLayout({
   return (
     <>
       <Script
+        id="google-analytics-src-page"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="google-analytics-page">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
